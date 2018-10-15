de648ee850ff552b18902a9d5d7808eedfd8ff58
Gwayway <952698119@qq.com>
2018-10-15 09:24:16 +0800

    Initial commit

---
+++ src/main/java/willbest/moneygot/MoneygotApplication.java
@@ -0,0 +1,12 @@
+package willbest.moneygot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneygotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneygotApplication.class, args);
+    }
+}
---
+++ src/test/java/willbest/moneygot/MoneygotApplicationTests.java
@@ -0,0 +1,16 @@
+package willbest.moneygot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneygotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
