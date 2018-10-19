1b967ebd92f6758a2774097341a02a23a2eb0248
Gwayway <952698119@qq.com>
2018-10-19 16:37:57 +0800

    1.0

--- src/main/java/willbest/moneygot/service/userservice.java
+++ src/main/java/willbest/moneygot/service/userservice.java
@@ -1,5 +1,12 @@
 package willbest.moneygot.service;
 import org.springframework.stereotype.Service;
+import willbest.moneygot.mapper.usermapper;
+
+import javax.annotation.Resource;
+
 @Service
 public class userservice {
+    @Resource
+    usermapper usermapper;
+
 }
